65b514d9e408ef941c492122bd78bbb74f5f4313
razzlestorm <willhite.jerimiah@gmail.com>
2022-10-20 22:55:22 -0700

    add tests and progress with web requests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
